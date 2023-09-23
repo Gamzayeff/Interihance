fdb89fac60a0a18306df4749a72fbea9d4a6671a
Notebook <Notebook@DESKTOP-QCDM367>
2023-09-23 12:18:07 +0400

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,12 @@
+﻿using Backup;
+
+Flash f1 = new Flash("USB", "rt", 2, 234);
+DVD d1 = new DVD("DVD-123", "lkt", 8, 34);
+HDD h1 = new HDD("Intel", "x5", 5, 47);
+
+//f1.PrintDeviceInfo();
+
+//d1.DownloadMedia();
+//d1.showFreeMemory();
+
+//h1.Copy();
